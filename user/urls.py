60046ab2d0d4bb8c40fad4ec682207b87809313c
Admin <admin@Nikolas-MacBook-Pro.local>
2022-05-12 11:14:15 +0200

    task/django serializers #CSC3-33

---
+++ user/urls.py
@@ -0,0 +1,11 @@
+from rest_framework import routers
+from django.urls import path, include
+from user import views
+
+router = routers.DefaultRouter()
+
+router.register('users', viewset=views.UserViewSet)
+
+urlpatterns = [
+    path('',include(router.urls))
+]
