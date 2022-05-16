41bbbd115615495e2c5f7ebdc0ad6278cdd5abe7
Admin <admin@Nikolas-MacBook-Pro.local>
2022-05-16 22:20:58 +0200

    task/django-permissions #CSC3-35

--- user/views.py
+++ user/views.py
@@ -8,24 +8,41 @@ from .serializers import UserSerializer
 from .models import User
 
 from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
-
-class UserViewSet(viewsets.ModelViewSet):
+# class UserViewSet(viewsets.ModelViewSet):
     
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#     queryset = User.objects.all()
 
-    permission_classes = [IsAuthenticated]
+#     permission_classes = [IsAuthenticated]
 
-    def retrieve(self, request, pk=None):
-        if request.user and pk == 'me':
-            return Response(UserSerializer(request.user).data)
-        return super(UserViewSet, self).retrieve(request, pk)
+#     def retrieve(self, request, pk=None):
+#         if request.user and pk == 'me':
+#             return Response(UserSerializer(request.user).data)
+#         return super(UserViewSet, self).retrieve(request, pk)
 
 class MyObtainTokenPairView(TokenObtainPairView):
     permission_classes = (AllowAny,)
     serializer_class = MyTokenObtainPairSerializer
+
+from rest_framework import mixins
+
+class UserViewSet(mixins.CreateModelMixin,
+                                mixins.ListModelMixin,
+                                mixins.RetrieveModelMixin,
+                                viewsets.GenericViewSet):
+    
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    
+    def get_permissions(self):
+        permission_classes = [AllowAny]
+        if self.action == 'retrieve':
+            permission_classes = [IsAdminUser]
+        
+        return [permission() for permission in permission_classes]
+    
 
 
     
